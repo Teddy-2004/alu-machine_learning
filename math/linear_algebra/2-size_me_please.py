06d8502116301f685ce30c214a1e4351720e55d1
Tedla Tesfaye <t.godebo@alustudent.com>
2026-01-28 18:49:59 +0200

    matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,5 +5,5 @@ def matrix_shape(matrix):
     num_cols = len(matrix[0])
     if isinstance(matrix[0][0], list):
         num_depth = len(matrix[0][0])
-        return (num_rows, num_cols, num_depth)
-    return (num_rows, num_cols)
+        return [num_rows, num_cols, num_depth]
+    return [num_rows, num_cols]
